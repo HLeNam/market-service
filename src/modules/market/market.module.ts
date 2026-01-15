00dc916e7cf1618af3db4a5991ba6353e698de4a
22120217 <hoanglenam0905@gmail.com>
2026-01-15 22:17:15 +0700

    chore(market): update market module configuration

--- src/modules/market/market.module.ts
+++ src/modules/market/market.module.ts
@@ -1,4 +1,32 @@
-import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
+import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CandleEntity } from 'src/modules/market/entities/candle.entity';
+import { MarketService } from './market.service';
+import { MarketController } from './market.controller';
+import { BinanceModule } from 'src/modules/binance/binance.module';
+import { CacheModule } from 'src/modules/cache/cache.module';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([CandleEntity]),
+    BullModule.registerQueue({
+      name: 'candle-storage',
+      defaultJobOptions: {
+        attempts: 3,
+        backoff: {
+          type: 'exponential',
+          delay: 1000,
+        },
+        removeOnComplete: true,
+        removeOnFail: false,
+      },
+    }),
+    forwardRef(() => BinanceModule),
+    CacheModule,
+  ],
+  providers: [MarketService],
+  controllers: [MarketController],
+  exports: [MarketService],
+})
 export class MarketModule {}
