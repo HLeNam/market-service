7a8bf4c4a249b37e7249dacaf74f4da64163a02b
22120217 <hoanglenam0905@gmail.com>
2026-01-15 23:00:36 +0700

    feat(health): add health check for socket

--- src/health/health/health.module.ts
+++ src/health/health/health.module.ts
@@ -3,9 +3,10 @@ import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
 import { HealthController } from './health.controller';
 import { CacheModule } from 'src/modules/cache/cache.module';
+import { BinanceModule } from 'src/modules/binance/binance.module';
 
 @Module({
-  imports: [TerminusModule, HttpModule, CacheModule],
+  imports: [TerminusModule, HttpModule, CacheModule, BinanceModule],
   controllers: [HealthController],
 })
 export class HealthModule {}
